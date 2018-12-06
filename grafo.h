ff0023515364910a645d235404735630a5a43c30
Giaco <43766886+Giacocipo@users.noreply.github.com>
2018-12-05 22:46:13 -0300

    Add files via upload

--- grafo.h
+++ grafo.h
@@ -18,15 +18,44 @@ class Grafo {
 		Aeropuerto* aeropuerto[MAX];
 
 	public:
+		//pre: -
+		//pos: Inicializa la cantidad de vertices en 0
 		Grafo();
+
+		//pre: -
+		//pos: Devuelve la cantidad de vertices
 		int obtener_cantidad();
+
+		//pre: Recibe un entero indice que representa a un aeropuerto
+		//pos: Devuelve una lista con los viajes al aeropuerto
 		list<Viaje> obtener_viajes(int indice);
+
+		//pre: Recibe un entero indice que representa a un aeropuerto
+		//pos: Devuelve una variable de tipo aeropuerto con los datos
 		Aeropuerto* obtener_aeropuerto(int indice);
+
+		//pre: Resive un string con el codigo IATA
+		//pos: Devuelve el indice del aeropuerto
 		int obtener_indice(string iata);
+
+		//pre: Resive le indice del aeropuerto de partida, el indice del aeropuerto destino y el peso del viaje
+		//pos: Agrega el viaje a la lista de viajes del aeropuerto
 		void agregar_viaje(int origen, int destino, int peso);
+
+		//pre: Resive un aeropuerto
+		//pos: Agrega el aeropuerto al grafo
 		int agregar_aeropuerto(Aeropuerto* aeropuerto);
+
+		//pre: -
+		//pos: Lista todos los viajes de cada aeropuerto en el grafo
 		void mostrar();
+
+		//pre: -
+		//pos: Lista todos los aeropuertos presentes en el grafo
 		void mostrar_aeropuertos();
+
+		//pre: -
+		//pos: Libera la memori dinamica utilizada
 		~Grafo();
 };
 
